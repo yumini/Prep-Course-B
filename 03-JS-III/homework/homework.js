f7b6cde32026dc0bd062efe367111c0c96c6dd5a
Yumi <yumi.tominaga@buildsoft.com.pe>
2022-09-15 20:21:06 -0500

    ytg

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,63 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var num=1;
+    var sum=0;
+    while(num<=10){
+      sum+=num;
+      num++;
+    }
+    return sum;
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var pares=[];
+  var cont=0;
+  for (numero of array) {
+    if (numero % 2 === 0){
+      pares[cont] = numero;
+      cont++;
+    }
+  }
+  return pares;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  /*
+  var array_cuadrado=[];
+  var cont=0;
+  for (numero of array) {
+    array_cuadrado[cont] = numero**2;
+    cont++;
+  }
+  return array_cuadrado;*/
+  array.forEach(function (elemento, indice) {
+    array[indice]=elemento**2;
+  });
+  return array;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var suma=0;
+  array.forEach(function (elemento, indice) {
+    suma+=elemento;
+  });
+  return suma;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  return num.toString().length;
 }
   
   
