bf67fe06514d5c0d7c90535b7af385ced548a000
Yumi <yumi.tominaga@buildsoft.com.pe>
2022-09-23 21:51:18 -0500

    ytg

--- 06-JS-VI/homework/homework.js
+++ 06-JS-VI/homework/homework.js
@@ -4,7 +4,7 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
-  return nombre.toUpperCase();
+  return nombre[0].toUpperCase()+ nombre.slice(1)
 }
 
 function invocarCallback(cb) {
@@ -25,12 +25,22 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
-}
+  var suma=0;
+  for(num of numeros){
+    suma+=num;
+  }
+  cb (suma);
+  
+
+  }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  for(var i=0; i<array.length; i++){
+    cb(array[i]);
+  }
 }
 
 function map(array, cb) {
@@ -38,12 +48,25 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var array_nuevo= array.map(function(x){
+    return cb(x);
+  })
+  return array_nuevo;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  var encontrados=[];
+  
+  for (element of array){
+      if (element[0]==='a'){
+        encontrados.push(element);
+      }
+  }
+  
+  return encontrados;
 }
 
 // No modificar nada debajo de esta línea
